18ff130e1dfcf09038397b48aba94dfb79caa561
Jan Graichen <jg@altimos.de>
2014-04-06 12:36:07 +0200

    Switch to Thor for CLI

---
+++ lib/stic/cli.rb
@@ -0,0 +1,45 @@
+require 'thor'
+
+module Stic
+
+  #
+  class CLI < ::Thor
+    package_name 'stic'
+
+    desc 'build', 'Build site'
+    def build
+      unless (site = Stic::Site.lookup)
+        puts 'Not in a stic site. You need to run the ' \
+             '`stic` command within a stic site.'
+        exit 1
+      end
+
+      STDOUT.puts  " Configuration files: #{site.config.files.join(", ")}"
+      STDOUT.puts  "              Source: #{site.source}"
+      STDOUT.puts  "              Target: #{site.target}"
+      STDOUT.print '       Generating ... '
+      STDOUT.flush
+
+      str_length = 0
+
+      log = lambda do |str|
+        STDOUT.print "\r                      " + ' ' * str_length
+        STDOUT.print "\r                  ... #{str}"
+        STDOUT.flush
+        str_length = str.length
+      end
+
+      begin
+        site.run {|generator| log.call "Running #{generator.class.name}" }
+        site.write {|blob| log.call "Write #{blob.relative_url}" }
+        site.cleanup
+      rescue => e
+        STDOUT.puts
+        raise e
+      end
+
+      log.call 'done.'
+      STDOUT.puts
+    end
+  end
+end
--- stic.gemspec
+++ stic.gemspec
@@ -18,10 +18,9 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ['lib']
 
-  spec.add_dependency 'commander'
+  spec.add_dependency 'thor'
   spec.add_dependency 'tilt'
   spec.add_dependency 'activesupport', '>= 4'
-  spec.add_dependency 'safe_yaml'
 
   spec.add_development_dependency 'bundler', '~> 1.3'
 end
